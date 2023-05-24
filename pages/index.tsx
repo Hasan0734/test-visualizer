b67e814beb8cfecd992dcbcf95b1a1cdf977ffb2
Jahid Hasan <jahidhasan2856@gmail.com>
2023-05-24 13:21:26 +0600

    added heading tag style on css

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
    <div>
-    <h1 className='text-3xl text-red-700 text-center'>Hello world</h1>
+    <h1 className='h1'>Hello world</h1>
    </div>
   )
 }

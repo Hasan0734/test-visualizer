b91b901711cdb23e2145c06b01c59e3f96712879
Jahid Hasan <jahidhasan2856@gmail.com>
2023-05-24 13:55:10 +0600

    added layout part

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+
+type LayoutProps = { children?: ReactNode }
+const Layout = ({ children }: LayoutProps) => {
+    return (
+        <>
+            <Navbar />
+            {children}
+            <Footer/>
+        </>
+    );
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,53 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar/Navbar'
+// import BlogsSection from '@/components/BlogsSection/BlogsSection';
+// import HeroSection from '@/components/HeroSection/HeroSection';
+// import Inspiring from '@/components/Inspiring/Inspiring';
+import Layout from '@/components/Layout/Layout';
+import React, { useEffect } from 'react';
+// import type { GetServerSideProps } from 'next';
+// import axios from 'axios';
+import { useAppDispatch } from '@/app/hooks';
+import { setBlogs } from '@/features/blogs/blogSlice';
+import { setCategories } from '@/features/categories/categoriesSlice';
 
 
-export default function Home() {
+export default function Home({ blogs, categories }:any) {
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(setBlogs(blogs))
+    dispatch(setCategories(categories))
+  },[])
+
   return (
-   <div>
-    <Navbar/>
-    <h1 className='card-title'>Hello world</h1>
-   </div>
+    <Layout>
+
+      {/* <HeroSection />
+      <BlogsSection/>
+      <Inspiring /> */}
+      <h1>Helo wrold</h1>
+    </Layout>
   )
 }
+
+// type dataType = {
+
+//   title: string;
+//   content: string;
+//   author: number;
+//   category: number;
+//   subcategory: number;
+//   banner: string;
+//   type: string;
+// }[];
+
+// export const getServerSideProps: GetServerSideProps<{
+//   blogs: dataType;
+// }> = async () => {
+
+//   const blogs: any = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/blogs`)
+//   const categories: any = await axios.get(`${process.env.NEXT_PUBLIC_HOST}/api/categories`)
+
+
+
+//   return { props: { blogs: blogs.data.data, categories: categories.data.data } };
+// };
+

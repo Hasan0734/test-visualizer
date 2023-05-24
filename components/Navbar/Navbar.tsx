ef634e0e23837c584fd19ce3572e9cceeea9e97d
Jahid Hasan <jahidhasan2856@gmail.com>
2023-05-24 13:49:21 +0600

    added components

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+         <h1 className='blog-title'>Hello world 2</h1>   
+        </>
+    );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar/Navbar'
 
 
 export default function Home() {
   return (
    <div>
+    <Navbar/>
     <h1 className='card-title'>Hello world</h1>
    </div>
   )

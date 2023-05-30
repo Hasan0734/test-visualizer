a4924204b57aa18ed7f9e657e5a3c5b0715b8b9c
Jahid Hasan <jahidhasan2856@gmail.com>
2023-05-30 23:29:30 +0600

    added existed header

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import Header from '../Header';
+import { menuItems } from '@/constants/header';
 
 type LayoutProps = { children?: ReactNode }
 const Layout = ({ children }: LayoutProps) => {
     return (
         <>
-            <Navbar />
+            <Header menuItems={menuItems} />
             {children}
             <Footer/>
         </>
